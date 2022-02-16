f273b92db77b1835f10ab7a29439a678fd5c6d11
zeekster3 <zeeekster3@gmail.com>
2022-02-16 09:42:27 -0700

    07 coding

--- Repeat.py
+++ Repeat.py
@@ -1,6 +1,6 @@
 number = int(input("Please type a positive number: "))
 
-while number < 0: 
+while number <= 0: 
     print("Sorry, that is a negative number. Please try again.")
     number = int(input("Please type a postive number:"))  
 print(f"The number is: {number}")
