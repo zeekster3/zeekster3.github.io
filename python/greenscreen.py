5b4e8bb4627a01a69d56b2de72833dc22a775ae8
zeekster3 <zeeekster3@gmail.com>
2022-02-26 21:55:32 -0700

    08 prove

--- python/greenscreen.py
+++ python/greenscreen.py
@@ -1,4 +1,17 @@
 from PIL import Image
-image_original = Image.open("beach.jpg")
 
-image_original.show()
+beach = Image.open("beach.jpg")
+cactus = Image.open("cactus.jpg")
+
+cactus_pixel = cactus.load()
+beach_pixel = beach.load()
+(width, height) = cactus.size
+
+for y in range(0, height):
+    for x in range(0, width):
+        r, g, b = cactus_pixel[x, y]
+        if r <= 70 and g >= 250 and b <= 25: 
+            cactus_pixel[x, y] = beach_pixel[x, y]
+
+cactus.show()
+cactus.save("beach_with_cactus.jpg")
